2362fe3dd7b01e7fe4276198f61dd903060950ea
Yura <hvalovyura@gmail.com>
2020-04-08 16:52:32 +0300

    Adding configuration (startup)

--- ProjectList/ProjectList/Startup.cs
+++ ProjectList/ProjectList/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjectList.Models;
 
 namespace ProjectList
 {
@@ -23,6 +25,8 @@ namespace ProjectList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = "Server = (localdb)\\mssqllocaldb; Database = userstoredb; Trusted_Connection = true";
+            services.AddDbContext<ProjectsContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
