6f3f92f91702207a19982d7b29f8351fddda5f87
Yura <hvalovyura@gmail.com>
2020-04-10 18:38:38 +0300

    Adding selection field (create project)

--- ProjectList/ProjectList/Controllers/HomeController.cs
+++ ProjectList/ProjectList/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectList.Controllers
             this.db = context;
         }
 
-        public IActionResult Index() //Min page with project list
+        public IActionResult Index() //Main page with project list
         {
             IQueryable<Project> users = db.Projects.Include(x => x.Category);
             return View(users);
@@ -26,6 +26,8 @@ namespace ProjectList.Controllers
 
         public IActionResult Create()
         {
+            IQueryable<Category> categories = db.Categories;
+            ViewBag.Greeting = categories;
             return View("Create");
         }
 
