c91c9a74ed8e2a42672037d1586d9d7a695a1b3d
Yura <hvalovyura@gmail.com>
2020-04-10 17:03:13 +0300

    Replacing methods in HomeController

--- ProjectList/ProjectList/Controllers/HomeController.cs
+++ ProjectList/ProjectList/Controllers/HomeController.cs
@@ -18,16 +18,10 @@ namespace ProjectList.Controllers
             this.db = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() //Min page with project list
         {
             IQueryable<Project> users = db.Projects.Include(x => x.Category);
             return View(users);
-        }
-
-        public IActionResult CategoryList()
-        {
-            IQueryable<Category> categories = db.Categories;
-            return View(categories);
         }
 
         public IActionResult Create()
@@ -42,6 +36,33 @@ namespace ProjectList.Controllers
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            Project project = await db.Projects.FirstOrDefaultAsync(p => p.Id == id);
+            if (project != null)
+            {
+                return View(project);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Project project)
+        {
+            db.Projects.Update(project);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult CategoryList()
+        {
+            IQueryable<Category> categories = db.Categories;
+            return View(categories);
+        }
+        
         public IActionResult CreateCategory()
         {
             return View("CreateCategory");
@@ -73,23 +94,7 @@ namespace ProjectList.Controllers
             return RedirectToAction("CategoryList");
         }
 
-        public async Task<IActionResult> Edit(int? id)
-        {
-            Project project = await db.Projects.FirstOrDefaultAsync(p => p.Id == id);
-            if (project != null)
-            {
-                return View(project);
-            }
-            return NotFound();                
-        }
 
-        [HttpPost]
-        public async Task<IActionResult> Edit(Project project)
-        {
-            db.Projects.Update(project);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
 
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         //public IActionResult Error()
